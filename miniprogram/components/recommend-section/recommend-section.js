c0ff23654b78011a7fd73ca1d22474f1923119d4
liqiantu <867899639@qq.com>
2020-09-26 23:25:02 +0800

    优化首页section组件

---
+++ miniprogram/components/recommend-section/recommend-section.js
@@ -0,0 +1,30 @@
+// components/recommend-section/recommend-section.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		sectionName: {
+			type: String,
+			value: ""
+		},
+		showFooter: {
+			type: Boolean,
+			value: false
+		}
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+
+	}
+})
